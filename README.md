5b0944e4f0d4bcde09073d204cf59ba6d9869d2e
limafresh <173814432+limafresh@users.noreply.github.com>
2025-05-15 12:06:22 +0300

    Rewritten on Web

--- README.md
+++ README.md
@@ -1,45 +1,26 @@
 # Math trainer  
-![Static Badge](https://img.shields.io/badge/Python-3.x-blue)
-![Static Badge](https://img.shields.io/badge/License-GPL_v3-blue)
-![Static Badge](https://img.shields.io/badge/PyQt-6-green)
-![Static Badge](https://img.shields.io/badge/Made_with-Qt_Designer-green)
-![Static Badge](https://img.shields.io/badge/Translated_with-Qt_Linguist-green)
-[![Ruff](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/astral-sh/ruff/main/assets/badge/v2.json)](https://github.com/astral-sh/ruff)
 
-A math trainer for kids written in Python and PyQt 6. It includes addition within 100 and multiplication within 10.
+An online math trainer for kids. It includes addition within 100 and multiplication within 10.
+
+Launch: ➡️ https://limafresh.github.io/MathTrainer/
 
 ![screenshot](https://raw.githubusercontent.com/limafresh/MathTrainer/main/screenshot.png)
-
-## Installation
-### Linux
-
-#### From .deb file (Debian/Ubuntu/Linux Mint)
-[Download .deb file](https://github.com/limafresh/MathTrainer/releases)
-
-| Release filename | Build system | Compatible distributions |
-| ---------------- | ------------ | ------------------------------------- |
-| `math-trainer_<version>-noble_all.deb` | Ubuntu Noble | Ubuntu Noble (24.04), Debian bookworm (12), Linux Mint 22, others based on Ubuntu 24.04 or Debian 12 |
-
-#### From PPA (Ubuntu based distributions, 24.04 and newer)
-```bash
-sudo add-apt-repository ppa:limafresh/math-trainer
-sudo apt update
-sudo apt install math-trainer
-```
-### Run via Python (without installation)
-Just run *math_trainer/main.py* via Python.
 
 ## How to use  
 The application generates an example with random numbers. You need to enter the correct answer to it and click the "CHECK" button.
 
-## Additional information
-`translations`: translation files created in Qt Linguist
+## Languages
+- English
+- Ukrainian
+- Russian
 
-`math_trainer`:
-+ `main.py`: contains the application logic
-+ `ui.ui`: contains the application interface, created in Qt Designer
-+ `locales`: contain translation, created as a result of conversion from *.ts* by the *lrelease* utility
-+ `sounds`: contain sounds of correct and wrong answers
+In the future we will add other languages. You can ask to add the desired language in Disscussions, I will add it faster.
 
 ## Credits
 Sound effects authors: *Fupy*, *0new4y*, under CC0; wood texture is by *SpringySpringo*, under CC0. From [OpenGameArt.org](https://opengameart.org/).
+
+## License
+MIT (GPLv3 for old PyQt6-based version).
+
+## Avialible everywhere: on smartphone, on tablet, on PC
+There are no barriers to learning mathematics. Previously it was written in PyQt6, but I rewrote it in Web for versatility.
---
+++ script.js
@@ -0,0 +1,141 @@
+// Functions
+
+function updateLanguage(language) {
+	const t = translations[language];
+	document.title = t.title;
+	document.getElementById("inputField").placeholder = t.placeholder;
+	document.getElementById("checkButton").textContent = t.checkButton;
+	document.getElementById("skipButton").textContent = t.skipButton;
+	document.getElementById("skippedExamples").textContent = t.skippedExamples;
+	document.getElementById("solvedExamples").textContent = t.solvedExamples;
+	document.getElementById("mistakes").textContent = t.mistakes;
+	document.getElementById("add100").textContent = t.add100;
+	document.getElementById("multiplication10").textContent = t.multiplication10;
+	document.getElementById("correctlyh1").textContent = t.correctly;
+	document.getElementById("wronglyh1").textContent = t.wrongly;
+}
+
+function resetCounters() {
+	skipped = 0;
+	solved = 0;
+	mistakes = 0;
+	document.getElementById("example").textContent = a + sign + b + "=";
+	document.getElementById("inputField").value = "";
+	document.getElementById("skippedExamples").textContent = skippedText + " " + skipped
+	document.getElementById("solvedExamples").textContent = solvedText + " " + solved
+	document.getElementById("mistakes").textContent = mistakesText + " " + mistakes
+};
+
+function nextExample() {
+	a = Math.floor((Math.random() * 90 + 10) / variable);
+	b = Math.floor((Math.random() * 90 + 10) / variable);
+
+	document.getElementById("example").textContent = a + sign + b + "=";
+	document.getElementById("inputField").value = "";
+}
+
+document.getElementById("select").addEventListener("change", function(event) {
+	const value = Number(event.target.value);
+	if (value === 0) {
+		sign = "+";
+		variable = 1;
+	} else {
+		sign = "*";
+		variable = 10;
+	};
+
+	a = Math.floor((Math.random() * 90 + 10) / variable);
+	b = Math.floor((Math.random() * 90 + 10) / variable);
+
+	resetCounters();
+});
+
+document.getElementById("checkButton").addEventListener("click", function() {
+	let correctAnswer;
+	if (sign === "+") {
+		correctAnswer = a + b;
+	} else {
+		correctAnswer = a * b;
+	};
+
+	if (Number(document.getElementById("inputField").value) === correctAnswer) {
+		document.getElementById("correctlySound").play();
+		document.getElementById("correctly").style.visibility = "visible";
+		setTimeout(() => {
+			document.getElementById("correctly").style.visibility = "hidden";
+		}, 1000);
+		solved++;
+		document.getElementById("solvedExamples").textContent = solvedText + " " + solved;
+		nextExample();
+	} else if (document.getElementById("inputField").value === "") {
+	} else {
+		document.getElementById("wronglySound").play();
+		document.getElementById("wrongly").style.visibility = "visible";
+		setTimeout(() => {
+			document.getElementById("wrongly").style.visibility = "hidden";
+		}, 1000);
+		mistakes++;
+		document.getElementById("mistakes").textContent = mistakesText + " " + mistakes;
+		nextExample();
+	}
+});
+
+document.getElementById("skipButton").addEventListener("click", function() {
+	skipped++;
+	document.getElementById("skippedExamples").textContent = skippedText + " " + skipped;
+	nextExample();
+});
+
+// App logic
+
+const translations = {
+	ru: {
+		title: "Математический тренажёр",
+		placeholder: "Введите тут, сколько будет...",
+		checkButton: "ПРОВЕРИТЬ",
+		skipButton: "Пропустить этот пример",
+		skippedExamples: "Пропущено примеров:",
+		solvedExamples: "Решено примеров:",
+		mistakes: "Ошибок:",
+		add100: "Сложение в пределах 100",
+		multiplication10: "Умножение в пределах 10",
+		correctly: "Правильно",
+		wrongly: "Неправильно",
+	},
+	uk: {
+		title: "Математичний тренажер",
+		placeholder: "Введіть тут, скільки буде...",
+		checkButton: "ПЕРЕВІРИТИ",
+		skipButton: "Пропустити цей приклад",
+		skippedExamples: "Пропущено прикладів:",
+		solvedExamples: "Розв'язано прикладів:",
+		mistakes: "Помилок:",
+		add100: "Додавання в межах 100",
+		multiplication10: "Множення в межах 10",
+		correctly: "Правильно",
+		wrongly: "Неправильно",
+	}
+}
+
+const languageSelect = document.getElementById("languageSelect");
+
+const browserLanguage = navigator.language.slice(0, 2);
+
+if (translations[browserLanguage]) {
+	updateLanguage(browserLanguage);
+};
+
+let a = Math.floor(Math.random() * 90) + 10;
+let b = Math.floor(Math.random() * 90) + 10;
+let sign = "+";
+let variable = 1;
+
+let skipped = 0;
+let solved = 0;
+let mistakes = 0;
+
+const skippedText = document.getElementById("skippedExamples").textContent
+const solvedText = document.getElementById("solvedExamples").textContent
+const mistakesText = document.getElementById("mistakes").textContent
+
+resetCounters();
